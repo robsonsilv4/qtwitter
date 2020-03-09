ffaa291b19b734fe6ca8a23c074e9fb7ad15b093
robsonsilv4 <robsonsilv410@gmail.com>
2020-03-09 14:24:17 -0300

    :sparkles: adicionado appbar

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,45 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: _appBar(context),
+      body: _body(),
     );
   }
+
+  Widget _appBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 1.0,
+      leading: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: CircleAvatar(
+          radius: 20,
+          backgroundImage: NetworkImage(
+            'https://picsum.photos/200',
+          ),
+        ),
+      ),
+      title: Text(
+        'Página Incial',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ),
+      ),
+      actions: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10.0,
+            vertical: 8.0,
+          ),
+          child: Icon(
+            Icons.info,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _body() => Container();
 }
